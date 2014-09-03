8dd7b12bdcdcf903cd30149e966420379c6ea727
Gyuri Horak <dyuri at horak dot hu>
2014-09-03 15:40:43 +0200

    Multiline quotes

--- bible_web.py
+++ bible_web.py
@@ -6,7 +6,7 @@
 # public domain, csinalsz vele amit akarsz
 
 from flask import Flask, jsonify, request, render_template
-from bible import get_random_line, get_bible_line, get_book_number
+from bible import get_random_line, get_bible_lines, get_book_number
 
 app = Flask(__name__)
 DEFAULT_LANG = 'hu'
@@ -24,6 +24,7 @@ def show_line(line, template_name='bibleline.html'):
     else:
         return render_template(template_name, line=line)
 
+
 @app.route("/prlx")
 @app.route("/prlx/<lang>/")
 def parallax_random_quote(lang=DEFAULT_LANG):
@@ -34,6 +35,7 @@ def parallax_random_quote(lang=DEFAULT_LANG):
 
     line = get_random_line(lang)
     return show_line(line, 'bibleline_parallax.html')
+
 
 @app.route("/")
 @app.route("/random/")
@@ -48,17 +50,19 @@ def random_quote(lang=DEFAULT_LANG):
     return show_line(line)
 
 
-@app.route("/<book>/<chapter>:<line>/")
-@app.route("/<book>/<chapter>:<line>/<lang>")
-def quote(book, chapter, line, lang=DEFAULT_LANG):
+@app.route("/<book>/<int:chapter>:<int:line>/")
+@app.route("/<book>/<int:chapter>:<int:line>/<lang>")
+@app.route("/<book>/<int:chapter>:<int:line>::<int:lineTo>/")
+@app.route("/<book>/<int:chapter>:<int:line>::<int:lineTo>/<lang>")
+def quote(book, chapter, line, lineTo=None, lang=DEFAULT_LANG):
     """
     Display a specific bible quote
     """
     lang = getlang(lang)
     book = get_book_number(book)
 
-    line = get_bible_line(book, chapter, line, lang)
-    return show_line(line)
+    lines = get_bible_lines(book, chapter, line, lineTo, lang, "<p>%s</p>")
+    return show_line(lines)
 
 
 if __name__ == "__main__":
